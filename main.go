73c2a72b082151a996ac39b15a5394b756ab7631
Fall1ngStar <titom98@live.fr>
2018-09-22 22:12:43 +0200

    remove libgit

--- main.go
+++ main.go
@@ -3,16 +3,10 @@ package main
 import (
 	"github.com/fall1ngstar/builder/project"
 	"github.com/urfave/cli"
-	"gopkg.in/libgit2/git2go.v26"
 	"log"
 	"os"
 	"fmt"
 )
-
-func createProject(path string) {
-	os.Mkdir(path, 0644)
-	git.InitRepository(path, false)
-}
 
 func main() {
 	app := cli.NewApp()
@@ -26,9 +20,9 @@ func main() {
 			Action: project.CreateProject,
 		},
 		{
-			Name: "check",
+			Name:  "check",
 			Usage: "Check requirements for project creation",
-			Action: func (c *cli.Context) error {
+			Action: func(c *cli.Context) error {
 				fmt.Println("Serverless:", project.CheckExecutableInPath("serverless"))
 				fmt.Println("Python:", project.CheckExecutableInPath("python"))
 				fmt.Println("NPM:", project.CheckExecutableInPath("npm"))
